f0a937cfa997c6d2c8857e7aeebaf0de9309a379
mingyifan2023 <150598503+mingyifan2023@users.noreply.github.com>
2024-09-29 00:11:09 +0900

    Update LogExporter.cpp

--- 1.cpp输出日志/LogExporter.cpp
+++ 1.cpp输出日志/LogExporter.cpp
@@ -1,1 +1,14 @@
+#include <fstream>
+#include <string>
 
+bool ExportLog(const std::string& logMessage) {
+    std::ofstream logFile("log.txt", std::ios::app);
+    if (logFile.is_open()) {
+        logFile << logMessage << std::endl; 
+        logFile.close(); 
+        return true; 
+    }
+    else {
+        return false; 
+    }
+}
